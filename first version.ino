411967c6d75481323fb8c6e258d4e08a2108e865
Itay Volk <itay.volk1@gmail.com>
2025-08-04 17:51:19 -0700

    initial commit

---
+++ first version.ino
@@ -0,0 +1,76 @@
+const int pins[1][2] = {{13, 12}};
+const int sensors[1] = {3};
+const int clock = 2;
+
+long * lengths;
+int index = 0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(clock, OUTPUT);
+  lengths = calloc(sizeof(sensors)/sizeof(int), sizeof(long));
+  for (int i = 0; i < sizeof(sensors)/sizeof(int); i++) {
+    for (int j = 0; j < 2; j++) {
+      pinMode(pins[i][j], OUTPUT);
+    }
+    pinMode(sensors[i], INPUT);
+    lengths[i] = 0;
+  }
+}
+
+// rotate the motor
+void move(int dir, int step, int motor[2]) {
+  digitalWrite(motor[1],dir); // Enables the motor to move in a particular direction
+  for(int x = 0; x < step; x++) {
+    digitalWrite(motor[0],HIGH); 
+    delayMicroseconds(500); 
+    digitalWrite(motor[0],LOW); 
+    delayMicroseconds(500); 
+  }
+}
+
+long* read(int pins[], int count) {
+  long * out = calloc(count, sizeof(long));
+
+  //read 24 bits
+  for (int i = 0; i < 24; i++) {
+    digitalWrite(clock, HIGH);
+    digitalWrite(clock, LOW);
+    for (int i = 0; i < count; i++) {
+      out[i] = out[i]*2 + digitalRead(sensors[i]);
+    }
+  }
+
+  // convert the binary format
+  for (int i = 0; i < count; i++) {
+    out[i] ^= 0x800000;
+  }
+
+  for (char i = 0; i < 3; i++) {
+    digitalWrite(clock, HIGH);
+    digitalWrite(clock, LOW);
+  }
+  return out;
+}
+
+const int step = 200;
+const int thres = 100;
+
+void loop() {
+  while(Serial.available()) {
+    lengths[index++] = Serial.parseInt();
+    if (index == sizeof(sensors)/sizeof(int)) {
+      index = 0;
+    }
+  }
+
+  long * out = read(sensors, sizeof(sensors)/sizeof(int));
+  for (int i = 0; i < sizeof(sensors)/sizeof(int); i++) {
+    if (out[i] > lengths[i]+thres) {
+      move(LOW, step, pins[i]);
+    } else if (out[i] < lengths[i]-thres) {
+      move(HIGH, step, pins[i]);
+    }
+  }
+  delay(1000);
+}
