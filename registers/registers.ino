85cfddf6e1ba604f6ac36603f26d2a1204391c57
Itay Volk <itay.volk1@gmail.com>
2025-08-06 11:20:14 -0700

    fixed register setup

--- registers/registers.ino
+++ registers/registers.ino
@@ -2,11 +2,11 @@ const int motorsData = 13;
 const int motorsEn = 12;
 const int motorsLatch = 11;
 const int motorsClock = 10;
-const int clocks[4] = {4, 3, 2, 1};
-const int clockEn = 5;
-const int data = 6;
+const int clocks[4] = {5, 4, 3, 2};
+const int clockEn = 6;
+const int data = 7;
 
-const int count = 3;
+const int count = 6;
 const int step = 200;
 const int thres = 100;
 
@@ -14,61 +14,36 @@ long * lengths = calloc(count, sizeof(long));
 int index = 0;
 
 void setup() {
-    Serial.begin(9600);
-    pinMode(motorsData, OUTPUT);
-    pinMode(motorsEn, OUTPUT);
-    digitalWrite(motorsEn, HIGH);
-    pinMode(motorsLatch, OUTPUT);
-    pinMode(motorsClock, OUTPUT);
+  Serial.begin(9600);
+  pinMode(motorsData, OUTPUT);
+  pinMode(motorsEn, OUTPUT);
+  digitalWrite(motorsEn, HIGH);
+  pinMode(motorsLatch, OUTPUT);
+  pinMode(motorsClock, OUTPUT);
 
-    pinMode(clockEn, OUTPUT);
-    pinMode(data, INPUT);
-    for (int i = 0; i < 4; i++) {
-        pinMode(clocks[i], OUTPUT);
-    }
-
-    long * out = read();
-    int dirs[count];
-    int steps[count];
-    for (int i = 0; i < count; i++) {
-        steps[i] = step+1;
-        if (!lengths[i]) {
-            dirs[i] = 0;
-            continue;
-        }
-
-        if (out[i] > lengths[i]+thres) {
-            dirs[i] = LOW;
-            steps[i] = 1;
-        } else if (out[i] < lengths[i]-thres) {
-            dirs[i] = HIGH;
-            steps[i] = 1;
-        }
-    }
-
-    move(dirs, steps);
+  pinMode(clockEn, OUTPUT);
+  pinMode(data, INPUT);
+  for (int i = 0; i < 4; i++) {
+      pinMode(clocks[i], OUTPUT);
+  }
 }
 
-// rotate the motor
+// rotate the motors
 void move(int dirs[], int steps[]) {
-    for (int x = 0; x < step; x++) {
+    for (int x = 1; x <= step; x++) {
         int pattern = 1;
         for (int i = 0; i < count; i++) {
-        pattern = pattern*4 + dirs[i]*2 + !(x%steps[i]);
-        if (i%3 == 2) {
-            Serial.println(pattern*2, BIN);
-            digitalWrite(motorsLatch, LOW);
-            shiftOut(motorsData, motorsClock, MSBFIRST , pattern*2);
-            digitalWrite(motorsLatch, HIGH);
-            pattern = 1;
+            pattern = pattern*4 + !(x%steps[i])*2 + dirs[i];
+            if (i%3 == 2) {
+                digitalWrite(motorsLatch, LOW);
+                shiftOut(motorsData, motorsClock, MSBFIRST , pattern*2);
+                digitalWrite(motorsLatch, HIGH);
+                pattern = 1;
+            }
         }
-        }
-
         digitalWrite(motorsEn, LOW);
-        // Serial.println("on");
-        delayMicroseconds(250); 
+        delayMicroseconds(250);
         digitalWrite(motorsEn,HIGH);
-        // Serial.println("off");
         delayMicroseconds(250);
     }
 }
@@ -118,7 +93,7 @@ void loop() {
             cur = Serial.read();
         }
         lengths[index++] = val;
-        Serial.println(lengths[index - 1]);
+        Serial.println(val);
         if (index == count) {
             index = 0;
         }
@@ -142,6 +117,5 @@ void loop() {
             steps[i] = 1;
         }
     }
-
-  move(dirs, steps);
+    move(dirs, steps);
 }
