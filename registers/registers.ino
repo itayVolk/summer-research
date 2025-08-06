851fa87ed2ce8a139ba839c0f40035ef402a80a5
Itay Volk <itay.volk1@gmail.com>
2025-08-06 14:26:15 -0700

    added support for infinite sensors

--- registers/registers.ino
+++ registers/registers.ino
@@ -2,44 +2,49 @@ const int motorsData = 13;
 const int motorsEn = 12;
 const int motorsLatch = 11;
 const int motorsClock = 10;
-const int clocks[4] = {5, 4, 3, 2};
-const int clockEn = 6;
-const int data = 7;
 
-const int count = 6;
+const int sensorsData = 2;
+const int sensorsEn = 3;
+const int sensorsLatch = 4;
+const int sensorsClock = 5;
+const int data = 6;
+
+const int count = 2;
 const int step = 200;
 const int thres = 100;
 
-long * lengths = calloc(count, sizeof(long));
+long * lengths = calloc(count*3, sizeof(long));
 int index = 0;
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(motorsData, OUTPUT);
-  pinMode(motorsEn, OUTPUT);
-  digitalWrite(motorsEn, HIGH);
-  pinMode(motorsLatch, OUTPUT);
-  pinMode(motorsClock, OUTPUT);
+    Serial.begin(9600);
+    pinMode(motorsData, OUTPUT);
+    pinMode(motorsEn, OUTPUT);
+    digitalWrite(motorsEn, HIGH);
+    pinMode(motorsLatch, OUTPUT);
+    pinMode(motorsClock, OUTPUT);
 
-  pinMode(clockEn, OUTPUT);
-  pinMode(data, INPUT);
-  for (int i = 0; i < 4; i++) {
-      pinMode(clocks[i], OUTPUT);
-  }
+    pinMode(sensorsData, OUTPUT);
+    pinMode(sensorsEn, OUTPUT);
+    digitalWrite(sensorsEn, HIGH);
+    pinMode(sensorsLatch, OUTPUT);
+    pinMode(sensorsClock, OUTPUT);
+    pinMode(data, INPUT);
 }
 
-// rotate the motors
+// rotate the motor
 void move(int dirs[], int steps[]) {
     for (int x = 1; x <= step; x++) {
         int pattern = 1;
-        for (int i = 0; i < count; i++) {
-            pattern = pattern*4 + !(x%steps[i])*2 + dirs[i];
-            if (i%3 == 2) {
-                digitalWrite(motorsLatch, LOW);
-                shiftOut(motorsData, motorsClock, MSBFIRST , pattern*2);
-                digitalWrite(motorsLatch, HIGH);
-                pattern = 1;
-            }
+        for (int i = 0; i < count*3; i++) {
+          pattern = pattern*4 + !(x%steps[i])*2 + dirs[i];
+          if (i%3 == 2) {
+              digitalWrite(motorsLatch, LOW);
+              shiftOut(motorsData, motorsClock, MSBFIRST , pattern*2);
+              digitalWrite(motorsLatch, HIGH);
+              delayMicroseconds(50);
+              pattern = 1;
+          }
         }
         digitalWrite(motorsEn, LOW);
         delayMicroseconds(250);
@@ -49,40 +54,42 @@ void move(int dirs[], int steps[]) {
 }
 
 long* read() {
-    long * out = calloc(count, sizeof(long));
+    long * out = calloc(count*3, sizeof(long));
+    digitalWrite(sensorsLatch, LOW);
+    digitalWrite(sensorsData, HIGH);
+    digitalWrite(sensorsClock, HIGH);
+    digitalWrite(sensorsClock, LOW);
+    digitalWrite(sensorsData, LOW);
+    digitalWrite(sensorsLatch, HIGH);
 
-    for (int i = 0; i < count; i++) {
-            //select which sensor we're on
-            int temp = i+1;
-            for (int j = 0; j < 4; j++) {
-            digitalWrite(clocks[j], temp%2);
-            temp /= 2;
-        }
-
+    for (int i = 0; i < count*3; i++) {
         //read 24 bits
-        for (int i = 0; i < 24; i++) {
-            digitalWrite(clockEn, HIGH);
-            digitalWrite(clockEn, LOW);
-            for (int i = 0; i < count; i++) {
-                out[i] = out[i]*2 + digitalRead(data);
-            }
+        for (int j = 0; j < 24; j++) {
+            digitalWrite(sensorsEn, HIGH);
+            digitalWrite(sensorsEn, LOW);
+            out[i] = out[i]*2 + digitalRead(data);
         }
 
         // convert the binary format
-        for (int i = 0; i < count; i++) {
-            out[i] ^= 0x800000;
-        }
+        out[i] ^= 0x800000;
 
         // prep for next read
-        for (char i = 0; i < 3; i++) {
-            digitalWrite(clockEn, HIGH);
-            digitalWrite(clockEn, LOW);
+        for (int j = 0; j < 3; j++) {
+            digitalWrite(sensorsEn, HIGH);
+            digitalWrite(sensorsEn, LOW);
         }
+
+        //move sensor
+        digitalWrite(sensorsLatch, LOW);
+        digitalWrite(sensorsClock, HIGH);
+        digitalWrite(sensorsClock, LOW);
+        digitalWrite(sensorsLatch, HIGH);
     }
     return out;
 }
 
 void loop() {
+    long * out = read();
     while (Serial.available()) {
         long val = 0;
         char cur = Serial.read();
@@ -94,15 +101,15 @@ void loop() {
         }
         lengths[index++] = val;
         Serial.println(val);
-        if (index == count) {
+        if (index == count*3) {
             index = 0;
         }
     }
 
-    long * out = read();
-    int dirs[count];
-    int steps[count];
-    for (int i = 0; i < count; i++) {
+    
+    int dirs[count*3];
+    int steps[count*3];
+    for (int i = 0; i < count*3; i++) {
         steps[i] = step+1;
         if (!lengths[i]) {
             dirs[i] = 0;
