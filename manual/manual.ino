53fd4cb39e5f603c7d4dd361f3fba655d93930b3
Itay Volk <itay.volk1@gmail.com>
2025-08-27 14:38:37 -0700

    added manual control script and sole right bendy lid gcode

---
+++ manual/manual.ino
@@ -0,0 +1,43 @@
+const int pins[2] = {13, 12};
+
+void setup() {
+    Serial.begin(9600);
+    for (int j = 0; j < 2; j++) {
+      pinMode(pins[j], OUTPUT);
+    }
+}
+
+// rotate the motor
+void move(int dir, int step, int motor[2]) {
+  digitalWrite(motor[1],dir); // Enables the motor to move in a particular direction
+  for(int x = 0; x < step; x++) {
+    digitalWrite(motor[0],HIGH); 
+    delayMicroseconds(500); 
+    digitalWrite(motor[0],LOW); 
+    delayMicroseconds(500); 
+  }
+}
+
+int dir = 0;
+void loop() {
+    if (Serial.available()) {
+        char cur;
+        do {
+            cur = Serial.read();
+        } while(cur == '\n' || cur == 0xFFFFFFFF);
+        switch(cur) {
+            case 'u':
+                dir = 1;
+                break;
+            case 'd':
+                dir = -1;
+                break;
+            default:
+                dir = 0;
+        }
+        dir = (cur == 'u')*2-1;
+    }
+    if (dir) {
+        move((dir+1)/2, 200, pins);
+    }
+}
